851cbe541c3b031d15d224e86624c2b4684600b7
Alexandre Nicastro <kryndex@gmail.com>
2018-01-03 02:46:55 -0200

    *: update

--- packet.tf
+++ packet.tf
@@ -3,6 +3,11 @@ provider "packet" {
   auth_token = "${var.packet_api_key}"
 }
 
+# resource "packet_ssh_key" "key1" {
+#   name       = "terraform-1"
+#   public_key       = "${file("/Users/alexxnica/.ssh/id_rsa.pub")}"
+# }
+
 # Create a device and add it to the project
 resource "packet_device" "dev" {
   billing_cycle    = "${var.billing_cycle}"
@@ -18,14 +23,21 @@ resource "packet_device" "dev" {
   # Main Setup
   provisioner "remote-exec" {
     inline = [
-      "adduser --disabled-password --gecos '${var.to_create_name}' ${var.to_create_username}",
-      "echo '${var.to_create_username}:${var.to_create_temporary_password}' | chpasswd",
-      "usermod -aG sudo ${var.to_create_username}",
+      "adduser --debug --disabled-password --gecos '${var.to_create_name}' ${var.to_create_username}",
+      "echo '${var.to_create_username}:${var.to_create_temporary_password}' | chpasswd -v",
+      "usermod -vaG sudo ${var.to_create_username}",
       "apt-get update",
       # "apt-get upgrade -y",
-      "apt-get {remove,install} -y apparmor",
+      # "apt-get {remove,install} -y apparmor",
       # "for var in ${var.to_install_apt_packages}; do apt-get install -y $var; done",
+      "reboot --verbose",
     ]
+    connection {
+      agent = "true"
+      type = "ssh"
+      user = "root"
+      # password = "${packet_device.dev.root_password}"
+    }
   }
 
   # Setup Continuation
@@ -44,19 +56,19 @@ resource "packet_device" "dev" {
       # "apt-get install -y zip",
       # "apt-get install -y linux-headers-generic",
       # "apt-get install -y linux-headers-4.13.0-16-generic",
-      "pip{3} install --upgrade ${var.to_install_pip_packages}",
-      "wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb | apt-get install -f",
-      "sudo curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -",
+      "pip{3} install -v --upgrade ${var.to_install_pip_packages}",
+      "wget -v https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb | apt-get install -f",
+      "curl -vfsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -",
       "add-apt-repository 'deb http://download.virtualbox.org/virtualbox/debian ${var.lsb_release} contrib'",
       # "apt-get install -y virtualbox-5.2",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -",
-      "apt-key fingerprint 0EBFCD88",
+      "curl -vfsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -",
+      "apt-key -v fingerprint 0EBFCD88",
       "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu ${var.lsb_release} test'",
       "apt-get update",
-      "apt-get install -y {virtualbox-5.2,docker-ce}",
+      # "apt-get install -y {virtualbox-5.2,docker-ce}",
       "docker run hello-world",
       "groupadd docker",
-      "usermod -aG docker ${var.to_create_username}",
+      "usermod -vaG docker ${var.to_create_username}",
       "systemctl enable docker",
       # "pip install ansible",
       # "pip install jinja2",
@@ -65,7 +77,7 @@ resource "packet_device" "dev" {
       # "pip3 install jinja2",
       # "pip3 install docker-py",
       "echo 'export HOST=localhost' >> /home/${var.to_create_username}/.bashrc",
-      "mkdir -p {/etc/{docker/certs,systemd/system/docker.service.d},/home/${var.to_create_username}/{certs,.docker}}",
+      # "mkdir -pv {/etc/{docker/certs,systemd/system/docker.service.d},/home/${var.to_create_username}/{certs,.docker}}",
       "systemctl daemon-reload",
       "service docker restart",
       "touch /home/${var.to_create_username}/.cloud-warnings.skip",
@@ -80,6 +92,7 @@ resource "packet_device" "dev" {
     ]
 
     connection {
+      agent = "true"
       type = "ssh"
       user = "root"
       # password = "${packet_device.dev.root_password}"
@@ -90,20 +103,38 @@ resource "packet_device" "dev" {
   provisioner "file" {
     source      = "./files/daemon.json"
     destination = "/etc/docker/daemon.json"
+    connection {
+      agent = "true"
+      type = "ssh"
+      user = "root"
+      # password = "${packet_device.dev.root_password}"
+    }
   }
 
   # Transfer systemd drop-in units
   provisioner "file" {
     source      = "./files/docker.conf"
     destination = "/etc/systemd/system/docker.service.d/docker.conf"
+    connection {
+      agent = "true"
+      type = "ssh"
+      user = "root"
+      # password = "${packet_device.dev.root_password}"
+    }
   }
 
   # Reboot for the last time before handing the machine to the user
   provisioner "remote-exec" {
     inline = [
       "echo 'Finished! Rebooting for the last time...'",
-      "reboot"
+      "reboot --verbose"
     ]
+    connection {
+      agent = "true"
+      type = "ssh"
+      user = "root"
+      # password = "${packet_device.dev.root_password}"
+    }
   }
 
   # Notification to signal that the machine is ready to be used
@@ -111,5 +142,11 @@ resource "packet_device" "dev" {
     inline = [
       "echo 'Machine ready!'",
     ]
+    connection {
+      agent = "true"
+      type = "ssh"
+      user = "root"
+      # password = "${packet_device.dev.root_password}"
+    }
   }
 }
